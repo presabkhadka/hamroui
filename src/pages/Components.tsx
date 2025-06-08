9ebec1e4ee5aea0054cd5c0bf78aac315fe87f0c
Presab <presabkhadka30@gmail.com>
2025-06-09 00:11:14 +0545

    updt - component page layout

--- src/pages/Components.tsx
+++ src/pages/Components.tsx
@@ -4,12 +4,14 @@ export default function Components() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2 h-screen overflow-hidden">
       <div className="col-span-full">
-        <div className="top-0 sticky z-50 border-b-2">
+        <div className=" shadow-lg border-b-2 top-0 sticky z-50">
           <Navbar />
         </div>
         <div className="grid grid-cols-4 gap-2 h-full overflow-y-scroll">
-<div className="border-r p-4 col-span-1 overflow-y-scroll">components here</div>
-<div className="p-4 flex grow col-span-3">description here</div>
+          <div className="border-r p-4 col-span-1 overflow-y-scroll">
+            components here
+          </div>
+          <div className="p-4 flex grow col-span-3">description here</div>
         </div>
       </div>
     </div>
